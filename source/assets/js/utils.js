74081c0e42753deb32fd55f8c4e313cc12161ae0
ouali aghiles <ouali.aghiles@gmail.com>
2019-05-20 17:02:51 +0100

    Add select2 and rework gestProduit

--- source/assets/js/utils.js
+++ source/assets/js/utils.js
@@ -171,6 +171,26 @@ var Utils = (function () {
                   </div>`
       return html
     },
+    generateToggle: function (size, {
+      title:title, cls:cls, id:id, multi:multi, label:label
+    }) {
+      if(label) {
+        var val = label
+      } else {
+        val = "Activer l'ajout multiple"
+      }
+      var html = `<div class="col-md-${size}">
+                    <label for="t">${title}</label>
+                    <div class="custom-control custom-switch">
+                      <input 
+                        class="${cls} custom-control-input" 
+                        id="${id}" 
+                        type="checkbox" data-check="${multi}"/>
+                      <label class="custom-control-label" for="${id}">${val}</label>
+                    </div>
+                  </div>`
+      return html
+    },
     /**
      *
      * @param {*} cls
@@ -257,21 +277,30 @@ var Utils = (function () {
      * @param arr
      * @returns {string}
      */
-    generateListInput: function (size, cls, val, id, label, arr) {
+    generateListInput: function (size, cls, val, id, label, arr, s2) {
       var options = function (tab) {
         var el;
-        tab.forEach(function (opt) {
-          var h = `<option value = '${opt.val}'>${opt.label}</option>`
+        tab.forEach(function (val) {
+          var h = `<option value="${val}">${val}</option>`
           el = el + h
         })
         return el
       }
       var html = `<div class="form-group col-md-${size}">
                     <label for="${id}">${val}</label>
-                    <select id="${id}" class=" custom-select ${cls}">
+                    <select id="${id}" class=" custom-select ${cls}" name="${label}">
                       ${options(arr)}
                     </select>
                   </div>`
+
+      if(s2 === 'multiTags') {
+        html = `<div class="form-group col-md-${size}">
+                  <label for="${id}">${val}</label>
+                  <select id="${id}" class="form-control" width="100%">
+                    ${options(arr)}
+                  </select> 
+                </div>`
+      }
       return html
     },
     /**
